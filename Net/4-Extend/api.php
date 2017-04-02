a92eb508453d588a9876e7d9024905162f1608d3
3553x <3553x@users.noreply.github.com>
2017-04-02 01:38:57 +0100

    cleaned code up

--- Net/4-Extend/api.php
+++ Net/4-Extend/api.php
@@ -2,7 +2,6 @@
 	$options = array("command", "key", "file");
 	$secret = "a5f9ej69duzpqR0z"; //16 characters
 	$request_para = array_keys($_GET);
-	//var_dump(hash("sha256", $secret . "command=open&file=info"));
 	foreach($request_para as $para)
 		if(!in_array($para, $options))
 		{
@@ -12,18 +11,10 @@
 
 	if($_GET['command'] == "open")
 	{
-		//var_dump($_GET);
 		$query = urldecode($_SERVER['QUERY_STRING']);
 		$key = substr($query, 4, 64);
 		$request = substr($query, 69);
-		//var_dump($key);
-		//var_dump($request);
-		//var_dump($query);
-		//var_dump($_SERVER['QUERY_STRING']);
 		$expected = hash("sha256", $secret . $request);
-		//var_dump(hash("sha256", "abicommand=open&secret=2"));
-		//var_dump($query);
-		//var_dump(hash("sha256", "ab" . $query));
 		if($key != $expected)
 		{
 			echo "Invalid key";
